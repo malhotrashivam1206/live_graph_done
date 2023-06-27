2c65a053107deb90d44c3510cadaf5d0437201e9
malhotrashivam1206 <malhotrashivam1206@gmail.com>
2023-06-27 11:21:52 +0530

    done

---
+++ live candle2.py
@@ -0,0 +1,144 @@
+import json
+from datetime import datetime
+from pytz import timezone
+from time import sleep
+import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Output, Input
+import plotly.graph_objects as go
+from alice_blue import AliceBlue
+
+from pya3 import *
+
+alice = Aliceblue(user_id='AB093838', api_key='cy5uYssgegMaUOoyWy0VGLBA6FsmbxYd0jNkajvBVJuEV9McAM3o0o2yG6Z4fEFYUGtTggJYGu5lgK89HumH3nBLbxsLjgplbodFHDLYeXX0jGQ5CUuGtDvYKSEzWSMk')
+
+print(alice.get_session_id())  # Get Session ID
+
+lp = 0
+socket_opened = False
+subscribe_flag = False
+subscribe_list = []
+unsubscribe_list = []
+data_list = []  # List to store the received data
+df = pd.DataFrame(columns=["timestamp", "open", "high", "low", "close"])  # Initialize an empty DataFrame for storing the data
+
+
+def socket_open():
+    print("Connected")
+    global socket_opened
+    socket_opened = True
+    if subscribe_flag:
+        alice.subscribe(subscribe_list)
+
+
+def socket_close():
+    global socket_opened, lp
+    socket_opened = False
+    lp = 0
+    print("Closed")
+
+
+def socket_error(message):
+    global lp
+    lp = 0
+    print("Error:", message)
+
+
+def feed_data(message):
+    global lp, subscribe_flag, data_list
+    feed_message = json.loads(message)
+    if feed_message["t"] == "ck":
+        print("Connection Acknowledgement status: %s (Websocket Connected)" % feed_message["s"])
+        subscribe_flag = True
+        print("subscribe_flag:", subscribe_flag)
+        print("-------------------------------------------------------------------------------")
+        pass
+    elif feed_message["t"] == "tk":
+        print("Token Acknowledgement status: %s" % feed_message)
+        print("-------------------------------------------------------------------------------")
+        pass
+    else:
+        print("Feed:", feed_message)
+        if 'lp' in feed_message:
+            timestamp = datetime.now(timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S.%f')
+            feed_message['timestamp'] = timestamp
+            lp = feed_message['lp']
+            data_list.append(feed_message)  # Append the received data to the list
+        else:
+            print("'lp' key not found in feed message.")
+
+
+# Connect to AliceBlue
+
+# Socket Connection Request
+alice.start_websocket(socket_open_callback=socket_open, socket_close_callback=socket_close,
+                      socket_error_callback=socket_error, subscription_callback=feed_data, run_in_background=True,
+                      market_depth=False)
+
+while not socket_opened:
+    pass
+
+# Subscribe to Tata Motors
+subscribe_list = [alice.get_instrument_by_symbol('NSE', 'TATAMOTORS')]
+alice.subscribe(subscribe_list)
+print(datetime.now())
+sleep(10)
+print(datetime.now())
+
+# Create an empty figure for the animated candlestick graph
+fig = go.Figure()
+
+# Initialize Dash app
+app = dash.Dash(__name__)
+
+# Define the layout of the Dash app
+app.layout = html.Div([
+    dcc.Graph(id='live-candlestick-graph'),
+    dcc.Interval(id='graph-update-interval', interval=2000, n_intervals=0)
+], style={'width': '100%', 'height': '100vh', 'margin': '0', 'padding': '0'})
+
+# Define the callback function to update the graph
+@app.callback(Output('live-candlestick-graph', 'figure'),
+              Input('graph-update-interval', 'n_intervals'))
+def update_graph(n):
+    global df, data_list
+
+    # Check if there is new data
+    if len(data_list) > 0:
+        # Convert the received data list to a DataFrame
+        new_df = pd.DataFrame(data_list)
+
+        # Extract the relevant columns (timestamp, LTP)
+        new_df = new_df[["timestamp", "lp"]]
+
+        # Convert the timestamp column to datetime format
+        new_df["timestamp"] = pd.to_datetime(new_df["timestamp"], format='%Y-%m-%d %H:%M:%S.%f')
+
+        # Convert the LTP column to numeric format
+        new_df["lp"] = pd.to_numeric(new_df["lp"], errors='coerce')
+
+        # Set the timestamp column as the DataFrame index
+        new_df.set_index("timestamp", inplace=True)
+
+        # Resample the LTP data into OHLC format using a 5-second interval
+        resampled_data = new_df["lp"].resample("10S").ohlc()
+
+        # Append the resampled OHLC data to the existing DataFrame
+        df = df.append(resampled_data)
+
+        data_list = []  # Clear the data list
+
+    # Update the candlestick graph
+    fig = go.Figure(data=[go.Candlestick(x=df.index,
+                                         open=df['open'],
+                                         high=df['high'],
+                                         low=df['low'],
+                                         close=df['close'])])
+
+    return fig
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.run_server(debug=True)
